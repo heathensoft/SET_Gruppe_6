7d2d6ed209244c9b53b0afae3c8d4d03919d17a3
Frederik Dahl <frederikdahl13@gmail.com>
2024-09-20 17:47:38 +0200

    Imported API for APP user interface.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,42 @@
+
+// https://www.lwjgl.org/customize
+
 plugins {
     id("no.hiof.set.g6.common-conventions")
 }
 
+val lwjglVersion = "3.3.4"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else if (arch.startsWith("ppc"))
+                "natives-linux-ppc64le"
+            else if (arch.startsWith("riscv"))
+                "natives-linux-riscv64"
+            else "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } ->  "natives-macos"
+        arrayOf("Windows").any { name.startsWith(it) } -> "natives-windows"
+        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
+
 dependencies {
     implementation(project(":database"))
+    //implementation(files("lib/heathensoft-jlib-0.5.0.jar"))
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
 }
 
---
+++ buildSrc/README.md
@@ -0,0 +1,2 @@
+
+Build Script Plugins 
