1a6aedaaeea3eb3c82a86992cd78afa5a5e50f13
Mahmad <maxagumauri04@gmail.com>
2024-10-14 21:02:22 +0200

    Updated getUsersByHubId method

--- database/src/main/java/no/hiof/set/g6/db/MyJDBC.java
+++ database/src/main/java/no/hiof/set/g6/db/MyJDBC.java
@@ -1,11 +1,14 @@
 package no.hiof.set.g6.db;
+
+import no.hiof.set.g6.dtdb.LocalUser;
 import java.sql.*;
-import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyJDBC {
 
-    //Metode for å hente ut alle brukere i ett spesifikt system
-    public static void getUsersByHubId(int hubId) {
+    // Metode for å hente ut alle brukere i ett spesifikt system
+    public static List<LocalUser> getUsersByHubId(int hubId) {
         String url = "jdbc:mysql://127.0.0.1:3306/login_schema";
         String username = "root";
         String password = "HeiHei123";
@@ -16,6 +19,8 @@ public class MyJDBC {
                 "JOIN Hubs ON LocalUser.hub_id = Hubs.hub_id " +
                 "WHERE Hubs.hub_id = ?";
 
+        List<LocalUser> userList = new ArrayList<>();
+
         try (Connection connection = DriverManager.getConnection(url, username, password);
              PreparedStatement statement = connection.prepareStatement(query)) {
 
@@ -25,26 +30,38 @@ public class MyJDBC {
             // Kjøre spørringen
             ResultSet resultSet = statement.executeQuery();
 
-            // Hent ut og skriv ut data
+            // Hent ut og lagre brukerinformasjon i LocalUser-objekter
             while (resultSet.next()) {
                 String userName = resultSet.getString("user_name");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String role = resultSet.getString("role");
+                String roleStr = resultSet.getString("role");
+                LocalUser.Role role = LocalUser.Role.valueOf(roleStr.toUpperCase());
 
-                // Skriv ut brukerinformasjon
-                System.out.println("UserName: " + userName + ", First Name: " + firstName + ", Last Name: " + lastName + ", Role: " + role);
+                // Opprett LocalUser-objekt
+                LocalUser user = new LocalUser();
+                user.setUserName(userName);
+                user.setRole(role);
+
+                // Legg til brukeren i listen
+                userList.add(user);
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return userList;  // Returnerer listen med brukere
     }
 
+
+    //Bare en test main metode, eksempel på hvordan metoden kan bli kalt.
     public static void main(String[] args) {
         // Henter brukere for en spesifikk hub_id
-        getUsersByHubId(1);  // Endre til det ønskede hub_id
+        List<LocalUser> users = getUsersByHubId(1);  // Endre til ønsket hub_id
+
+        // Iterer gjennom listen og printer ut brukerinformasjon
+        for (LocalUser user : users) {
+            System.out.println("UserName: " + user.getUserName() +
+                    ", Role: " + user.getRole());
+        }
     }
 }
-
-
