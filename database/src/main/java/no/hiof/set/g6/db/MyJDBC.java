6da5a487f94156c41307e09c3152b77ef783aabc
Mahmad <maxagumauri04@gmail.com>
2024-10-16 20:07:34 +0200

    updated getAllUserInfo

--- database/src/main/java/no/hiof/set/g6/db/MyJDBC.java
+++ database/src/main/java/no/hiof/set/g6/db/MyJDBC.java
@@ -1,45 +1,51 @@
 package no.hiof.set.g6.db;
 
 import no.hiof.set.g6.dtdb.LocalUser;
+import org.json.simple.JSONObject;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
+import no.hiof.set.g6.dt.G6JSON;
+import no.hiof.set.g6.dt.HomeAddress;
+import no.hiof.set.g6.dt.UserAccount;
 
 public class MyJDBC {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Teknologiprosjekt";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "HeiHei123";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Teknologiprosjekt"; // Database URL
+    private static final String USERNAME = "root"; // Database username
+    private static final String PASSWORD = "HeiHei123"; // Database password
 
-    // Metode for å hente all informasjon om brukere med hub_id = 1
-    public static List<String> getAllUserInfo() {
+    // Method to retrieve all user information for users with hub_id = 1
+    public List<JSONObject> getAllUserInfo() {
+
+        // SQL query
         String query = """
             SELECT LocalUser.user_name, UserAccount.first_name, UserAccount.last_name, UserAccount.email,
                            HomeAddress.country, HomeAddress.state, HomeAddress.city, HomeAddress.street_address, HomeAddress.postal_code,
-                           LocalUser.role
-                    FROM LocalUser \s
-                    JOIN UserAccount ON LocalUser.account_id = UserAccount.account_id\s
+                           LocalUser.role, UserAccount.phone_numbers
+                    FROM LocalUser  
+                    JOIN UserAccount ON LocalUser.account_id = UserAccount.account_id 
                     JOIN HomeAddress ON UserAccount.address_id = HomeAddress.address_id
                     WHERE LocalUser.hub_id = 1;
-                    
         """;
 
-        List<String> userInfoList = new ArrayList<>();
+        List<JSONObject> userList = new ArrayList<>(); // List to store user information as JSON objects
 
         try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
              PreparedStatement statement = connection.prepareStatement(query)) {
 
-            // Kjører spørringen
+            // Execute the SQL query
             ResultSet resultSet = statement.executeQuery();
 
-            // Henter ut og lagrer informasjonen som JSON-strukturer i listen
+            // Loop through the result set
             while (resultSet.next()) {
                 String userName = resultSet.getString("user_name");
                 String firstName = resultSet.getString("first_name");
                 String lastName = resultSet.getString("last_name");
                 String email = resultSet.getString("email");
                 String country = resultSet.getString("country");
+                String phoneNumbers = resultSet.getString("phone_numbers");
+
                 String state = resultSet.getString("state");
                 String city = resultSet.getString("city");
                 String streetAddress = resultSet.getString("street_address");
@@ -47,31 +53,36 @@ public class MyJDBC {
                 String roleStr = resultSet.getString("role");
                 LocalUser.Role role = LocalUser.Role.valueOf(roleStr.toUpperCase());
 
-                // Bygger opp en streng med all informasjon
-                String userInfo = String.format(
-                        "{ \"userName\": \"%s\", \"firstName\": \"%s\", \"lastName\": \"%s\", \"email\": \"%s\", \"country\": \"%s\", \"state\": \"%s\", \"city\": \"%s\", \"streetAddress\": \"%s\", \"postalCode\": %d, \"role\": \"%s\" }",
-                        userName, firstName, lastName, email, country, state, city, streetAddress, postalCode, role
-                );
+                // Create address and user account objects
+                HomeAddress address = new HomeAddress(country, state, city, streetAddress, postalCode);
+                UserAccount account = new UserAccount(firstName, lastName, email);
+                account.getAddress().set(address);
+                account.getPhoneNumbers().add(phoneNumbers);
 
-                // Legger til informasjonen i listen
-                userInfoList.add(userInfo);
+                // Convert the UserAccount object to a JSON object
+                JSONObject jsonObject = G6JSON.userAccountToJSON(account);
+
+                // Add the JSON object to the userList
+                userList.add(jsonObject);
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Handle any SQL exceptions
         }
 
-        return userInfoList;  // Returnerer listen
+        return userList; // Return the list of JSON objects
     }
 
-    // Testmetode for å demonstrere hvordan man henter ut all brukerinformasjon
+    // Main method for testing the retrieval of user information
     public static void main(String[] args) {
-        // Henter all informasjon om brukere
-        List<String> userInfoList = getAllUserInfo();
+        MyJDBC myJDBC = new MyJDBC();
 
-        // Printer ut all brukerinformasjon
-        for (String userInfo : userInfoList) {
-            System.out.println(userInfo);
+
+        List<JSONObject> userInfoList = myJDBC.getAllUserInfo();
+
+
+        for (JSONObject userInfo : userInfoList) {
+            System.out.println(userInfo.toJSONString());
         }
     }
 }
