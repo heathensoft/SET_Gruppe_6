d75cea388fd3129a346a05b34e3483f71b9868a0
Frederik Dahl <frederikdahl13@gmial.com>
2024-10-21 00:25:00 +0200

    Database Interface

---
+++ database/src/main/java/no/hiof/set/g6/db/DemoDatabase.java
@@ -0,0 +1,52 @@
+package no.hiof.set.g6.db;
+
+import no.hiof.set.g6.ny.DatatypeArray;
+import no.hiof.set.g6.ny.LocalUser;
+import no.hiof.set.g6.ny.UserAccount;
+
+import java.nio.file.Path;
+
+public class DemoDatabase implements PrototypeDB {
+
+    private Path path;
+
+    @Override
+    public LocalUser.Role getUserRole(LocalUser user) {
+        return null;
+    }
+
+    @Override
+    public DatatypeArray<UserAccount> searchForAccount(UserAccount account) throws Exception {
+        return null;
+    }
+
+    @Override
+    public DatatypeArray<LocalUser> allStoredLocalUsers() throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean addLocalUser(LocalUser user) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean removeLocalUser(LocalUser user) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean editLocalUser(LocalUser user) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void load() throws Exception {
+        PrototypeDB.super.load();
+    }
+
+    @Override
+    public void save() throws Exception {
+        PrototypeDB.super.save();
+    }
+}
