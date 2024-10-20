8df497e7be7f3ba7c1223724d390adae47801bbc
Mahmad <maxagumauri04@gmail.com>
2024-10-20 23:34:43 +0200

    minor changes

--- datatype/src/main/java/no/hiof/set/g6/ny/LocalUser.java
+++ datatype/src/main/java/no/hiof/set/g6/ny/LocalUser.java
@@ -1,120 +1,98 @@
 package no.hiof.set.g6.ny;
 
-
 import org.json.simple.JSONObject;
 
-/**
- * @author Mahmad
- */
-
-
 public class LocalUser extends G6Datatype {
-    
-    public static final String JSON_KEY_ID = "User ID";
-    public static final String JSON_KEY_USER_NAME = "User Name";
-    public static final String JSON_KEY_USER_ROLE = "User Role";
-    public static final String JSON_KEY_USER_ACCOUNT = "User Account";
-    
+
     // ENUM for role in the system
     public enum Role {
-        
-        OWNER("Owner"),
-        RESIDENT("Resident"),
-        GUEST("Guest"),
-        NONE("None");
-        
-        Role(String descriptor) {
-            this.descriptor = descriptor;
-        }
-        public final String descriptor;
-        private static final Role[] all;
-        static { all = values(); }
-        public static Role getByOrdinal(int ordinal) {
-            if (ordinal < all.length && ordinal > 0) {
-                return all[ordinal];
-            } return null;
-        }
+        OWNER, RESIDENT, GUEST
     }
-    
-    private final UserAccount account;  // corresponds to account_id INT (Foreign Key)
-    public String userName;             // corresponds to user_name VARCHAR(100)
-    public Role role;                   // corresponds to role ENUM('Owner', 'Resident', 'Guest')
-    public int userID;                  // corresponds to local_user_id INT
-    
-    // Constructors, Getters, and Setters
+
+    // JSON keys
+    public static final String JSON_KEY_USER_ACCOUNT = "User Account";
+    public static final String JSON_KEY_USER_NAME = "User Name";
+    public static final String JSON_KEY_ROLE = "Role";
+
+    private UserAccount userAccount;
+    private String userName;
+    private Role role;
+
+    // Constructors
+    public LocalUser(UserAccount userAccount, String userName, Role role) {
+        this.userAccount = userAccount;
+        this.userName = userName;
+        this.role = role;
+    }
+
     public LocalUser() {
-        this.account = new UserAccount();
-        this.role = Role.NONE;
+        this.userAccount = new UserAccount();
         this.userName = "";
+        this.role = Role.GUEST;  // Default role
     }
-    
-    public UserAccount getAccount() { return account; }
-    
+
+    // Getters
+    public UserAccount getUserAccount() { return userAccount; }
+    public String getUserName() { return userName; }
+    public Role getRole() { return role; }
+
+    // Setters
+    public void setUserAccount(UserAccount userAccount) { this.userAccount = userAccount; }
+    public void setUserName(String userName) { this.userName = userName; }
+    public void setRole(Role role) { this.role = role; }
+
+    // Method to convert JSON to LocalUser object
     @Override
     public void fromJson(JSONObject jsonObject) throws Exception {
         if (jsonObject == null) throw new Exception("JSONObject is null");
-        Object userIDObject = jsonObject.get(JSON_KEY_ID);
-        Object roleObject = jsonObject.get(JSON_KEY_USER_ROLE);
+
+        Object userAccountObject = jsonObject.get(JSON_KEY_USER_ACCOUNT);
         Object userNameObject = jsonObject.get(JSON_KEY_USER_NAME);
-        Object accountObject = jsonObject.get(JSON_KEY_USER_ACCOUNT);
-        if (JsonUtils.anyObjectIsNull(
-                userIDObject,
-                roleObject,
-                accountObject,
-                userNameObject
-        )) throw new Exception("JSON to LocalUser: Missing one or more fields");
+        Object roleObject = jsonObject.get(JSON_KEY_ROLE);
+
+        if (JsonUtils.anyObjectIsNull(userAccountObject, userNameObject, roleObject))
+            throw new Exception("JSON to LocalUser: Missing one or more fields");
+
         try {
-            
-            Integer userID = (Integer) userIDObject;
+            JSONObject userAccountJson = (JSONObject) userAccountObject;
             String userName = (String) userNameObject;
-            Role userRole = Role.getByOrdinal((Integer) roleObject);
-            JSONObject jsonAccount = (JSONObject) accountObject;
-            
-            this.account.fromJson(jsonAccount);
+            String roleString = (String) roleObject;
+
+            this.userAccount.fromJson(userAccountJson);
             this.userName = userName;
-            this.role = userRole;
-            this.userID = userID;
-            
-        } catch (ClassCastException e) {
-            throw new Exception("JSON to LocalUser: Invalid format for one or more fields",e);
+            this.role = Role.valueOf(roleString.toUpperCase());
+        } catch (ClassCastException | IllegalArgumentException e) {
+            throw new Exception("JSON to LocalUser: Invalid format for one or more fields", e);
         }
     }
-    
+
+    // Method to convert LocalUser object to JSON
     @Override
     @SuppressWarnings("unchecked")
     public JSONObject toJson() {
-        UserAccount account = this.account;
-        Role role = this.role == null ? Role.NONE : this.role;
-        String userName = this.userName == null ? "null" : this.userName;
-        JSONObject jsonObject = new JSONObject(); {
-            jsonObject.put(JSON_KEY_ID,userID);
-            jsonObject.put(JSON_KEY_USER_NAME,userName);
-            jsonObject.put(JSON_KEY_USER_ROLE,role.ordinal());
-            jsonObject.put(JSON_KEY_USER_ACCOUNT,account.toJson());
-        } return jsonObject;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(JSON_KEY_USER_ACCOUNT, userAccount.toJson());
+        jsonObject.put(JSON_KEY_USER_NAME, userName == null ? "null" : userName);
+        jsonObject.put(JSON_KEY_ROLE, role == null ? Role.GUEST.name() : role.name());
+        return jsonObject;
     }
-    
-    // Just for sorting purposes
-    
+
+    // Method to compare LocalUser objects
     @Override
     public int compareTo(G6Datatype other) {
         if (other instanceof LocalUser o) {
             int comp;
             LocalUser t = this;
-            {
-                int t_role = t.role == null ? Role.NONE.ordinal() : t.role.ordinal();
-                int o_role = o.role == null ? Role.NONE.ordinal() : o.role.ordinal();
-                comp = Integer.compare(t_role,o_role);
+
+            comp = t.userName.compareTo(o.userName == null ? "" : o.userName);
+            if (comp == 0) {
+                comp = t.role.compareTo(o.role);
             }
             if (comp == 0) {
-                String t_last_name = t.userName == null ? "" : t.userName;
-                String o_last_name = o.userName == null ? "" : o.userName;
-                comp = t_last_name.compareTo(o_last_name);
-            }
-            if (comp == 0) {
-                comp = t.account.compareTo(o.account);
+                comp = t.userAccount.compareTo(o.userAccount);
             }
             return comp;
-        } return 0;
+        }
+        return 0;
     }
 }
