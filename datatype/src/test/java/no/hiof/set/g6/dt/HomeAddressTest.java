3df368e9c2e6a5c0fb55b0fedb2fc6d42a3537af
Mahmad <maxagumauri04@gmail.com>
2024-11-07 21:18:29 +0100

    Added JsonConversion test

--- datatype/src/test/java/no/hiof/set/g6/dt/HomeAddressTest.java
+++ datatype/src/test/java/no/hiof/set/g6/dt/HomeAddressTest.java
@@ -1,27 +1,39 @@
 package no.hiof.set.g6.dt;
 
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class HomeAddressTest {
 
 
-    /*
     @Test
-    @DisplayName("Assert Expected Values after HomeAddress set to \"null\"")
-    public void setNull() {
-        HomeAddress address = new HomeAddress();
-        address.set(null);
-        Assertions.assertEquals("null",address.country);
-        Assertions.assertEquals("null",address.state);
-        Assertions.assertEquals("null",address.city);
-        Assertions.assertEquals("null",address.streetAddress);
-        Assertions.assertEquals(0,address.postalCode);
+    @DisplayName("Test JSON serialization and deserialization for HomeAddress object equality")
+    public void testJsonConversion() throws Exception {
+        // Oppretter et HomeAddress-objekt
+        HomeAddress originalAddress = new HomeAddress();
+        originalAddress.country = "Norway";
+        originalAddress.state = "Viken";
+        originalAddress.city = "Oslo";
+        originalAddress.street = "Karl Johans gate";
+        originalAddress.postalCode = 0162;
+
+        // Konverterer til JSON
+        JSONObject jsonObject = originalAddress.toJson();
+
+        // Oppretter et nytt HomeAddress-objekt
+        HomeAddress newAddress = new HomeAddress();
+        newAddress.fromJson(jsonObject);
+
+        // Sjekker at de to objektene er like
+        assertEquals(originalAddress.country, newAddress.country);
+        assertEquals(originalAddress.state, newAddress.state);
+        assertEquals(originalAddress.city, newAddress.city);
+        assertEquals(originalAddress.street, newAddress.street);
+        assertEquals(originalAddress.postalCode, newAddress.postalCode);
     }
-
-
-     */
-  
 }
